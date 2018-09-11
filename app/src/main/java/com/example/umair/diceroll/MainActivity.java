add22f8f4be8f5ebb82b0097879fa96a13509900
Umair <umairmajeed719@gmail.com>
2018-09-11 18:30:27 +0500

    Committed

---
+++ app/src/main/java/com/example/umair/diceroll/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.umair.diceroll;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button rollButton;
+     ImageView leftDice ;
+     ImageView rightDice ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         rollButton = (Button) findViewById(R.id.btn_roll);
+         leftDice = (ImageView) findViewById(R.id.img_dice1);
+         rightDice = (ImageView) findViewById(R.id.img_dice2);
+
+             final int[] diceArray = {R.drawable.dice1,
+                                          R.drawable.dice2,
+                                            R.drawable.dice3,
+                                         R.drawable.dice4,
+                                         R.drawable.dice5,
+                                            R.drawable.dice6};
+         rollButton.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+
+                 Random randomNumberGenerator = new Random();
+                 int number = randomNumberGenerator.nextInt(6);
+                 int randNumber = randomNumberGenerator.nextInt(6);
+                 Log.d("DiceRoll","The number is "+ number);
+                 leftDice.setImageResource(diceArray[number]);
+                 rightDice.setImageResource(diceArray[randNumber]);
+             }
+         });
+
+
+    }
+}
